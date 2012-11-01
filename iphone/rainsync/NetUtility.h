c96e204b0920f292ddb7eb1243fd71ab6a2466d1
xorox90 <xorox90@nate.com>
2012-11-01 23:09:59 +0900

    Handler를 등록하여 API 호출 가능하게 수정

--- iphone/rainsync/NetUtility.h
+++ iphone/rainsync/NetUtility.h
@@ -7,16 +7,33 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Queue.h"
 
-@interface NetUtility : NSObject
+enum req_type{
+    account_register=0,
+    account_login=1,
+    
+};
+
+
+@interface NetUtility : NSObject <NSURLConnectionDelegate>
 {
     @private NSMutableData* responseData;
-    @private void (^ block)(NSData*);
+    //@private void (^ block)(NSData*);
+    
+    @private NSMutableArray* arr;
+    @private Queue *queue;
+    @private NSString *server;
     
 }
 @property (nonatomic, strong) NSMutableData* responseData;
-@property (nonatomic, strong) void (^block)(NSData*);
--(void) getURL:(NSString *)url withBlock:(void (^)(NSData*))block;
--(void) postURL:(NSString*)url withData:(NSData*)data withBlock:(void (^)(NSData*))block;
+@property (nonatomic, strong) void (^block)(int, NSDictionary*);
+-(id)initwithBlock:(void (^)(int, NSDictionary*))block;
 
+-(void) dealloc;
+//:(void (^)(NSData*))block
+-(void) getURL:(NSString *)url;
+-(void) postURL:(NSString*)url withData:(NSData*)data;
+-(void) account_registerwithAcessToken:(NSString*)accesstoken withNick:(NSString*)nick withPhoto:(NSString*)photo;
+-(void) end;
 @end
