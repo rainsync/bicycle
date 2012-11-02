93b75f4a67df0561e31149fcf8ece54886b58b2f
closer27 <closer527@gmail.com>
2012-11-03 02:52:48 +0900

    GCD 구현을 위한 소스 구현

--- SlowWorker/SlowWorker/ViewController.m
+++ SlowWorker/SlowWorker/ViewController.m
@@ -27,6 +27,34 @@
 }
 
 - (void)doWork:(id)sender {
-    
+    NSDate *startTime = [NSDate date];
+    NSString *fetchedData = [self fetchSomethingFromServer];
+    NSString *processedData = [self processData:fetchedData];
+    NSString *firstResult = [self calculateFirstResult:processedData];
+    NSString *secondResult = [self calculateSecondResult:processedData];
+    NSString *resultsSummary = [NSString stringWithFormat:@"First: [%@]\nSecond: [%@]", firstResult, secondResult];
+    _resultsTextView.text = resultsSummary;
+    NSDate *endTime = [NSDate date];
+    NSLog(@"Completed in %f seconds", [endTime timeIntervalSinceDate:startTime]);
+}
+
+- (NSString *)fetchSomethingFromServer {
+    [NSThread sleepForTimeInterval:1];
+    return @"Hi there";
+}
+
+- (NSString *)processData:(NSString *)data {
+    [NSThread sleepForTimeInterval:2];
+    return [data uppercaseString];
+}
+
+- (NSString *)calculateFirstResult:(NSString *)data {
+    [NSThread sleepForTimeInterval:3];
+    return [NSString stringWithFormat:@"Number of chars: %d", [data length]];
+}
+
+- (NSString *)calculateSecondResult:(NSString *)data {
+    [NSThread sleepForTimeInterval:4];
+    return [data stringByReplacingOccurrencesOfString:@"E" withString:@"e"];
 }
 @end
