66759e48b8e08dc58088f3ed4eab22418385819c
closer27 <closer527@gmail.com>
2012-11-03 02:43:58 +0900

    그랜드 센트럴 디스패치(GCD) 이용 샘플 프로젝트 생성

---
+++ SlowWorker/SlowWorker/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  SlowWorker
+//
+//  Created by 승원 김 on 12. 11. 3..
+//  Copyright (c) 2012년 승원 김. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)doWork:(id)sender {
+    
+}
+@end
