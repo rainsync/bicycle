eae1df7a3e9a6a49a96c6ceb80b8a9b9c363b418
closer27 <closer527@gmail.com>
2012-11-05 18:36:19 +0900

    최고속도 체크 기능 추가, 현재속도 -1(측정불가)일때 측정 불가 메세지 표기

--- Location_sample/Location_sample/ViewController.m
+++ Location_sample/Location_sample/ViewController.m
@@ -34,6 +34,8 @@
     isEnded = NO;
     timeCounter = 0;
     avgSpeed = 0.0f;
+    _averageSpeed.text = @"0";
+    _highestSpeed.text = @"0";
     
     _resumeRecordBtn.hidden = YES;
     _pauseRecordBtn.hidden = YES;
@@ -226,6 +228,7 @@
 }
 
 - (void)viewDidUnload {
+    [self setHighestSpeed:nil];
     [self setDbStatusLabel:nil];
     [self setSaveRidingBtn:nil];
     [self setWeightSlider:nil];
@@ -270,7 +273,18 @@
     
     NSString *currentSpeed = [[NSString alloc] initWithFormat:@"%g", newLocation.speed];
     NSString *kmhSpeed = [NSString stringWithFormat:@"%f", [currentSpeed floatValue] * 3.6];    // m/s -> km/h 변환
-    _currentSpeed.text = kmhSpeed;
+    
+    if ([currentSpeed floatValue] <= -1) {
+        _currentSpeed.text = @"Can't check";
+    }
+    else {
+        _currentSpeed.text = kmhSpeed;
+        
+        if ([kmhSpeed floatValue] > [_highestSpeed.text floatValue]) {
+            _highestSpeed.text = kmhSpeed;      // 최고 속도 반영
+        }
+        
+    }
     
     if (startLocation == nil) {
         self.startLocation = newLocation;
