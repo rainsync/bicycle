38fb4be032585f1392f004acd10727e728069477
closer27 <closer527@gmail.com>
2012-11-06 03:36:27 +0900

    네비게이션 스타일 일괄 조정 및 그에 따른 불필요한 코드 삭제

---
+++ Location_sample/Location_sample/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  Location_sample
+//
+//  Created by 승원 김 on 12. 11. 6..
+//  Copyright (c) 2012년 승원 김. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
--- iphone/rainsync/ViewController.m
+++ iphone/rainsync/ViewController.m
@@ -19,16 +19,18 @@
 {
     [super init];
     
+    [[UINavigationBar appearance] setBarStyle:UIBarStyleBlackTranslucent];
+    [[UINavigationBar appearance] setTintColor:[UIColor colorWithRed:0 green:.2 blue:.4 alpha:1.0f]];
+    
     //ridingViewController = [[RidingViewController alloc] initWithNibName:@"RidingViewController" bundle:nil];
     //[self addChildViewController:ridingViewController];
     
     RidingViewController *ridingViewController = [[RidingViewController alloc] initWithNibName:@"RidingViewController" bundle:nil];
-    //UINavigationController *ridingViewNavController = [[UINavigationController alloc] initWithRootViewController:ridingViewController];
+//    UINavigationController *ridingViewNavController = [[UINavigationController alloc] initWithRootViewController:ridingViewController];
 
     
     ProfileViewController* profile = [[ProfileViewController alloc] initWithNibName:@"ProfileViewController" bundle:nil];
     UINavigationController *profileNavController = [[UINavigationController alloc] initWithRootViewController:profile];
-    
 
     StaticViewController* statics = [[StaticViewController alloc]initWithNibName:@"StaticViewController" bundle:nil];
     UINavigationController *staticsNavController = [[UINavigationController alloc] initWithRootViewController:statics];
@@ -38,8 +40,8 @@
     
     // 네이비게이션 바가 필요한 프로필 탭, 통계 탭, 설정 탭은 네비게이션 컨트롤러에 뷰 삽입하여 탭바 컨트롤러에 삽임한다.
     self.viewControllers = @[ridingViewController, profileNavController,staticsNavController, settingNavController];
-    
-    
+
+   
     return self;
     
 }
