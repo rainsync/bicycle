a2fecebda753c582d20f09162b296c33da69fa19
xorox90 <xorox90@nate.com>
2012-11-05 17:30:47 +0900

    Riding DB skeleton code

---
+++ iphone/RidingDB.h
@@ -0,0 +1,16 @@
+//
+//  RidingDB.h
+//  rainsync
+//
+//  Created by xorox64 on 12. 11. 5..
+//  Copyright (c) 2012년 rainsync. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "sqlite3.h"
+
+@interface RidingDB : NSObject{
+    sqlite3 *ridingDB;
+    NSString *databasePath; // db파일 경로
+}
+@end
