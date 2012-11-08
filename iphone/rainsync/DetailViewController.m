c64ebf26e47daa1c1979e00e0046b8e56c0931c6
closer27 <closer527@gmail.com>
2012-11-09 03:00:55 +0900

    통계 상세뷰에서 기록측정 날짜를 보여주도록 구현

--- iphone/rainsync/DetailViewController.m
+++ iphone/rainsync/DetailViewController.m
@@ -35,4 +35,12 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)dealloc {
+    [_recordingDay release];
+    [super dealloc];
+}
+- (void)viewDidUnload {
+    [self setRecordingDay:nil];
+    [super viewDidUnload];
+}
 @end
