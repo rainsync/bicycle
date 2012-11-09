5ae21338410e30f4b512a3733a6162b09726db57
xorox90 <xorox90@nate.com>
2012-11-09 20:00:35 +0900

    #13 close 상세 지도 뷰 구현

--- iphone/rainsync/DetailViewController.h
+++ iphone/rainsync/DetailViewController.h
@@ -7,13 +7,20 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
-@interface DetailViewController : UIViewController
+@interface DetailViewController : UIViewController <MKMapViewDelegate>
+{
+    MKPolyline *line;
+    MKPolylineView *view;
+    
+}
 @property (retain, nonatomic) IBOutlet UILabel *recordingDay;
 @property (strong, nonatomic) IBOutlet UILabel *recordingTime;
 @property (strong, nonatomic) IBOutlet UILabel *distance;
 @property (strong, nonatomic) IBOutlet UILabel *altitude;
 @property (strong, nonatomic) IBOutlet UILabel *averageSpeed;
 @property (strong, nonatomic) IBOutlet UILabel *calorie;
+@property (retain, nonatomic) IBOutlet MKMapView *mapView;
 
 @end
--- iphone/rainsync/DetailViewController.m
+++ iphone/rainsync/DetailViewController.m
@@ -18,9 +18,76 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
+        view = [NSNull null];
         // Custom initialization
     }
     return self;
+}
+
+
+-(void)setMapCenter:(CLLocationCoordinate2D)location
+{
+    //[self.mapView setCenterCoordinate:location zoomLevel:13 animated:YES];
+    
+    
+    NSLog(@"Current Location : %f, %f",location.latitude,location.longitude);
+    MKCoordinateRegion region;
+    MKCoordinateSpan span;
+    span.latitudeDelta=0.01;
+    span.longitudeDelta=0.01;
+    region.span=span;
+    //location.latitude=(float *)(self.appDelegate.currentLocationLatitude);
+    //location.latitude=self.appDelegate.currentLocationLongitude;
+    region.center=location;
+    [self.mapView setRegion:region animated:TRUE];
+    [self.mapView regionThatFits:region];
+    
+    
+    
+}
+- (void)loadView:(NSMutableDictionary *)rowData
+{
+    self.recordingDay.text = [rowData objectForKey:@"day"];
+    self.recordingTime.text = [rowData objectForKey:@"time"];
+    self.distance.text = [rowData objectForKey:@"distance"];
+    self.averageSpeed.text = [rowData objectForKey:@"speed"];
+    self.altitude.text = [rowData objectForKey:@"altitude"];
+    self.calorie.text = [rowData objectForKey:@"calorie"];
+    
+    NSMutableArray * locations = [rowData objectForKey:@"locations"];
+    CLLocationCoordinate2D * coords = malloc([locations count]*sizeof(MKMapPoint));
+    
+    int i=0;
+    CLLocationCoordinate2D point;
+    CLLocationCoordinate2D mid;
+    //CLLocationCoordinate2D northEastPoint;
+    //CLLocationCoordinate2D southWestPoint;
+    
+    for (CLLocation *location in locations) {
+        point = location.coordinate;
+        if(i==0)
+            mid=point;
+        coords[i++] = point;
+        //point = MKMapPointForCoordinate(location.coordinate);
+
+        
+        //coords[i++]= point;
+
+            
+    }
+    
+    line = [MKPolyline polylineWithCoordinates:coords count:[locations count]];
+    [self.mapView addOverlay:line];
+    //point.latitude /= [locations count];
+    //point.longitude /= [locations count];
+    [self setMapCenter:mid];
+    
+    //MKMapRectMake(southWestPoint.x, southWestPoint.y, northEastPoint.x - southWestPoint.x, northEastPoint.y - southWestPoint.y);
+    
+    free(coords);
+    
+    
+    
 }
 
 - (void)viewDidLoad
@@ -42,6 +109,7 @@
     [_altitude release];
     [_averageSpeed release];
     [_calorie release];
+    [_mapView release];
     [super dealloc];
 }
 - (void)viewDidUnload {
@@ -51,6 +119,27 @@
     [self setAverageSpeed:nil];
     [self setAltitude:nil];
     [self setCalorie:nil];
+    [self setMapView:nil];
     [super viewDidUnload];
 }
+
+- (MKOverlayView *)mapView:(MKMapView *)mapView viewForOverlay:(id <MKOverlay>)overlay
+{
+	MKOverlayView* overlayView = nil;
+	
+    if(overlay == line){
+        if(view == [NSNull null])
+        {
+            view = [[MKPolylineView alloc] initWithPolyline:line];
+            view.fillColor = [UIColor redColor];
+            view.strokeColor = [UIColor redColor];
+            view.lineWidth = 3;
+        }
+        overlayView = view;
+    }
+
+    return overlayView;
+    
+}
+
 @end
