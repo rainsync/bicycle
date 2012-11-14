1f002f17c460e48b61323134726597698d4faec2
xorox90 <xorox90@nate.com>
2012-11-14 16:43:47 +0900

    로그인 모듈 만듬

---
+++ iphone/rainsync/Login.h
@@ -0,0 +1,13 @@
+//
+//  Login.h
+//  rainsync
+//
+//  Created by xorox64 on 12. 11. 14..
+//  Copyright (c) 2012년 rainsync. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Login : NSObject
+
+@end
---
+++ iphone/rainsync/Login.m
@@ -0,0 +1,13 @@
+//
+//  Login.m
+//  rainsync
+//
+//  Created by xorox64 on 12. 11. 14..
+//  Copyright (c) 2012년 rainsync. All rights reserved.
+//
+
+#import "Login.h"
+
+@implementation Login
+
+@end
